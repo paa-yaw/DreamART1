407c32c4c4dc4ad68b3db1f1756e8895c093de12
Derek <deepsky_5@live.com>
2014-11-15 03:24:17 +0000

    artwork model created

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,2 +1,3 @@
 class Artwork < ActiveRecord::Base
+	mount_uploader :picture, PictureUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :artworks
+  get 'pages/info'
 
+  resources :artworks
+  root :to => redirect('/artworks')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
